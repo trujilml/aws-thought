39e238cb9f8276182e031a0703beab052b40bd05
trujilml <mickeytrj@gmail.com>
2022-03-12 12:07:47 -0600

    modified initial files for aws table development, completed aws and insomnia route functionality by adding routes for all users, username, and thoughts to be posted

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -2,11 +2,12 @@ const AWS = require("aws-sdk");
 const fs = require('fs');
 
 AWS.config.update({
-    region: "us-east-2",
-    endpoint: "http://localhost:8000"
-});
+    region: 'us-east-2',
+  });
 
-const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const dynamodb = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10',
+  });
 
 console.log("Importing thoughts into DynamoDB. Please wait.");
 
